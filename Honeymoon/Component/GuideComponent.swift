28d70dd9ddac30f936ac6d71df4b8bbe28f1ed5c
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-03-12 14:40:48 +0500

    Added some propeeties along with the sample data n the preview on the guide component

---
+++ Honeymoon/Component/GuideComponent.swift
@@ -0,0 +1,28 @@
+//
+//  GuideComponent.swift
+//  Honeymoon
+//
+//  Created by Elexoft on 12/03/2025.
+//
+
+import SwiftUI
+
+struct GuideComponent: View {
+    // MARK: - PROPERTIES
+    var title: String
+    var subtitle: String
+    var description: String
+    var icon: String
+    
+    // MARK: - BODY
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+// MARK: - PREVIEW
+
+#Preview(traits: .sizeThatFitsLayout) {
+    GuideComponent(title: "Title", subtitle: "Swipe right", description: "This is a placeholder sentence. This is a placeholder sentence. This is a placeholder sentence.", icon: "heart.circle")
+}
