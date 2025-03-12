606b70a0d96a153fc4dc5aa66a41b9b12b6f56e6
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-03-12 14:27:02 +0500

    Header componnent new file is added to the rpoject

---
+++ Honeymoon/Component/HeaderComponent.swift
@@ -0,0 +1,18 @@
+//
+//  HeaderComponent.swift
+//  Honeymoon
+//
+//  Created by Elexoft on 12/03/2025.
+//
+
+import SwiftUI
+
+struct HeaderComponent: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview(traits: .fixedLayout(width: 375, height: 128)) {
+    HeaderComponent()
+}
