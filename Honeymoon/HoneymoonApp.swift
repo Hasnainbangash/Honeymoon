7847ba502940e54e8a7111d8c4c4d74b4e415197
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-03-12 11:51:38 +0500

    Initial Commit

---
+++ Honeymoon/HoneymoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoneymoonApp.swift
+//  Honeymoon
+//
+//  Created by Elexoft on 12/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct HoneymoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
