8a8ea71cc7f57ea28a4e07c9ca5dea204f5220cb
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-03-13 10:41:20 +0500

    Added the environment presentation mode and add the dismiis to the continue button to dismiss the sheet

--- Honeymoon/View/GuideView.swift
+++ Honeymoon/View/GuideView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct GuideView: View {
     // MARK: - PROPERTIES
+    @Environment(\.presentationMode) var presentationMode
     
     // MARK: - BODY
     
@@ -55,6 +56,7 @@ struct GuideView: View {
                 Button(action: {
                     // ACTION
                     print("A button was tapped.")
+                    self.presentationMode.wrappedValue.dismiss()
                 }) {
                     Text("Continue".uppercased())
                         .font(.headline)
