10575e470c2034e335628aa0dfb409a80f8c1a68
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-03-13 11:32:40 +0500

    Infi view designalmost work done

--- Honeymoon/View/InfoView.swift
+++ Honeymoon/View/InfoView.swift
@@ -23,17 +23,13 @@ struct InfoView: View {
                     .fontWeight(.black)
                     .modifier(TitleModifier())
                 
-                AppInfoView() //: HSTACK
+                AppInfoView()
                 
                 Text("Credits")
                     .fontWeight(.black)
                     .modifier(TitleModifier())
                 
-                HStack {
-                    Text("Photos").foregroundColor(Color.gray)
-                    Spacer()
-                    Text("Unsplash")
-                } //: HSTACK
+                CreditsView()
                 
                 Spacer(minLength: 10)
                 
@@ -61,10 +57,52 @@ struct InfoView: View {
 
 struct AppInfoView: View {
     var body: some View {
-        HStack {
-            Text("Application").foregroundColor(Color.gray)
-            Spacer()
-            Text("Honeymoon")
-        }
+        VStack(alignment: .leading, spacing: 10) {
+            RowAppInfoView(ItemOne: "Application", ItemTwo: "Honeymoon")
+            RowAppInfoView(ItemOne: "Compatibility", ItemTwo: "iPhone and iPad")
+            RowAppInfoView(ItemOne: "Developer", ItemTwo: "John / Jane")
+            RowAppInfoView(ItemOne: "Designer", ItemTwo: "Robert Petras")
+            RowAppInfoView(ItemOne: "Website", ItemTwo: "swiftuimasterclass.com")
+            RowAppInfoView(ItemOne: "Version", ItemTwo: "1.0.0")
+        } //: VSTACK
+    }
+}
+
+struct RowAppInfoView: View {
+    // MARK: - PROPERTIES
+    var ItemOne: String
+    var ItemTwo: String
+    
+    // MARK: - BODY
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Text(ItemOne).foregroundColor(Color.gray)
+                Spacer()
+                Text(ItemTwo)
+            } //: HSTACK
+            Divider()
+        } //: VSTACK
+    }
+}
+
+struct CreditsView: View {
+    var body: some View {
+        VStack(alignment: .leading, spacing: 10) {
+            HStack {
+                Text("Photos").foregroundColor(Color.gray)
+                Spacer()
+                Text("Unsplash")
+            } //: HSTACK
+            
+            Divider()
+            
+            Text("Photographers").foregroundColor(Color.gray)
+            
+            Text("Shifaaz Shamoon (Maldives), Grillot Edouard (France), Evan Wise (Greece), Christoph Schulz (United Arab Emirates), Andrew Coelho (USA), Damiano Baschiera (Italy), Daniel Olah (Hungary), Andrzej Rusinowski (Poland), Lucas Miguel (Slovenia), Florencia Potter (Spain), Ian Simmonds (USA), Ian Keefe (Canada), Denys Nevozhai (Thailand), David Köhler (Italy), Andre Benz (USA), Alexandre Chambon (South Korea), Roberto Nickson (Mexico), Ajit Paul Abraham (UK), Jeremy Bishop (USA), Davi Costa (Brazil), Liam Pozz (Australia)")
+                .multilineTextAlignment(.leading)
+                .font(.footnote)
+        } //: VSTACK
     }
 }
