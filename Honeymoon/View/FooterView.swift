681ec991f8d87593bb34a54199e6aa100f8dc830
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-03-14 15:38:50 +0500

    Added the haptics to the footer view on the book destination button

--- Honeymoon/View/FooterView.swift
+++ Honeymoon/View/FooterView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct FooterView: View {
     // MARK: - PROPERTIES
     @Binding var showBookingAlert: Bool
+    let haptics = UINotificationFeedbackGenerator()
     
     // MARK: - BODY
     
@@ -24,6 +25,7 @@ struct FooterView: View {
                 // ACTION
                 print("Success!")
                 playSound(sound: "sound-click", type: "mp3")
+                self.haptics.notificationOccurred(.success)
                 self.showBookingAlert.toggle()
             }) {
                 Text("Book Destination".uppercased())
