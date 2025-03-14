fd52b9f56151de0fdd95245c782ab64e9f268efd
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-03-14 15:23:35 +0500

    Added the transiotion to the content view cardview and on chnage changing the ttransition and removal accordingly

--- Honeymoon/Extension/CardTransition.swift
+++ Honeymoon/Extension/CardTransition.swift
@@ -11,6 +11,7 @@ extension AnyTransition {
     // The main reason to use the asymmetric transition because we only want to animate the cards transition when it's removed. So when a new card is inserted in the deck, there should be no animation what so ever.
     
     // The first trailing bottom transition is used when the card is thrown away to the right of the screen
+    // This creates a "swipe away to the bottom-right" effect
     static var trailingBottom: AnyTransition {
         AnyTransition.asymmetric(
             insertion: .identity,
@@ -18,6 +19,7 @@ extension AnyTransition {
     }
     
     // The second leading bottom transition is applied when the card is thrown away to the left
+    // This creates a "swipe away to the bottom-left" effect
     static var leadingBottom: AnyTransition {
         AnyTransition.asymmetric(
             insertion: .identity,
