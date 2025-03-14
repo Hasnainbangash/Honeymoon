cb154033968f5be9017ec6212e1ce773f3362f97
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-03-14 11:21:22 +0500

    THE ROATION EFFECT FOR THE CARD VIEW IS ADDED AND WORK DONE

--- Honeymoon/View/ContentView.swift
+++ Honeymoon/View/ContentView.swift
@@ -88,6 +88,7 @@ struct ContentView: View {
                         .zIndex(isTopCard(cardView: cardView) ? 1 : 0)
                         .offset(x: self.dragState.translation.width, y: self.dragState.translation.height)
                         .scaleEffect(self.dragState.isDragging ? 0.85 : 1.0)
+                        .rotationEffect(Angle(degrees: Double(self.dragState.translation.width / 12)))
                         .animation(.interpolatingSpring(stiffness: 120, damping: 120))
                         .gesture(
                             LongPressGesture(minimumDuration: 0.01)
